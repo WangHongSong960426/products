8e5c6ff1d78a46d34ed05dba438310f7576984c3
Wayne Wang <hsw960426@gmail.com>
2022-07-05 09:59:46 +0800

    202207050958

--- products.py
+++ products.py
@@ -1,12 +1,22 @@
-#讀取檔案
+import os 
+
 products = []
-with open('products.csv', 'r', encoding= 'utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+    print('找到了檔案')
+    with open('products.csv', 'r', encoding= 'utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+
+else:
+    print('找不到檔案')
+    
+
+#讀取檔案
+
 
 #請使用者輸入
 while True:
@@ -24,6 +34,6 @@ for p in products:
     
 #寫入檔案
 with open('products.csv', 'w', encoding= 'utf-8') as f:
-    f.write('商品, 價格\n')
+    f.write('商品,價格\n')
     for p in products:
         f.write(p[0] + ',' + str(p[1]) + '\n')
